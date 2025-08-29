5b9e92cb49337c42c69512d68507fbe44a09e6a9
Tirzah Gill <tirzalegit@gmail.com>
2025-08-30 02:41:09 +0500

    Implement POST endpoint to add new student

--- main.py
+++ main.py
@@ -26,3 +26,17 @@ def read_student(id: int):
         if id == student['id']:
             return student
     raise HTTPException(status_code=404, detail="Student not found")
+
+def next_id():
+    if not students:
+        return 1
+    max_id = max(s['id'] for s in students)
+    return max_id + 1
+
+@app.post("/students/", response_model=Student, status_code=201)
+def create_student(student: Student):
+    student_obj = student.model_dump(exclude_none=True)
+    student_obj['id'] = next_id()
+    students.append(student_obj)
+    students.sort(key=lambda s: s["id"])
+    return student_obj
