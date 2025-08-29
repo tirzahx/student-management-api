0f7c50916134435164dafc8cf51056efe59a8bab
Tirzah Gill <tirzalegit@gmail.com>
2025-08-30 02:37:34 +0500

    Add initial sample students

--- main.py
+++ main.py
@@ -9,3 +9,9 @@ class Student(BaseModel):
     age: int
     gender: str
     email: str
+
+students = [
+    {'id': 1, 'name': 'Saim', 'age': 30, 'gender': 'male', 'email': 'saim@example.com'},
+    {'id': 2, 'name': 'Hamza', 'age': 24, 'gender': 'male', 'email': 'hamza@example.com'},
+    {'id': 3, 'name': 'Alishba', 'age': 27, 'gender': 'female', 'email': 'alishba@example.com'}
+]
